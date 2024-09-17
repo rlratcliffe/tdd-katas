ee432e5c2fe1418a64620031705ed9b78a2f6f65
Rob Ratcliffe <rlr@tcliffe.dev>
2024-09-16 21:42:48 -0400

    add height to plateau

--- mars-rovers/python-mars-rovers/python_mars_rovers/plateau.py
+++ mars-rovers/python-mars-rovers/python_mars_rovers/plateau.py
@@ -1,6 +1,10 @@
 class Plateau:
-	def __init__(self, xbound):
+	def __init__(self, xbound, ybound):
 		self.xbound = xbound
+		self.ybound = ybound
 
 	def getWidth(self):
 		return self.xbound
+
+	def getHeight(self):
+		return self.ybound
--- mars-rovers/python-mars-rovers/tests/test_plateau.py
+++ mars-rovers/python-mars-rovers/tests/test_plateau.py
@@ -1,4 +1,7 @@
 from python_mars_rovers.plateau import Plateau
 
-def test_init():
-	assert Plateau(5).getWidth() == 5
+def test_plateau_getWidth():
+	assert Plateau(5, 2).getWidth() == 5
+
+def test_plateau_getHeight():
+	assert Plateau(5, 3).getHeight() == 3
