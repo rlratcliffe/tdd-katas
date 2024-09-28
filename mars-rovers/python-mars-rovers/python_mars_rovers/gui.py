748ea810534c891dfd6c472ce7bebb606cc3dde6
Rob Ratcliffe <rlr@tcliffe.dev>
2024-09-27 21:25:46 -0400

    add sample of gui implementation

--- mars-rovers/python-mars-rovers/python_mars_rovers/gui.py
+++ mars-rovers/python-mars-rovers/python_mars_rovers/gui.py
@@ -1,10 +1,19 @@
 from nicegui import ui
 from plateau import Plateau
 
-plateau = Plateau(5, 4)
+plateau = Plateau(5, 3)
 height = plateau.getHeight()
 width = plateau.getWidth()
 
 ui.label('The plateau height is ' + str(height))
+
+with ui.grid(columns=height, rows=width):
+    for x in reversed(range(0, width)):
+        for y in range(0, height):
+            if x == 0 and y == 1:
+                ui.label('🦊').classes('border p-1')
+            else:
+                ui.label('').classes('border p-1')
+    
 
 ui.run()
