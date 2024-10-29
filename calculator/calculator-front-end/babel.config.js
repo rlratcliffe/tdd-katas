69d386b9e95eb4770af5c67626a6270e7fee2eae
Rob Ratcliffe <rlr@tcliffe.dev>
2024-10-28 22:28:45 -0400

    fix build warning

--- calculator/calculator-front-end/babel.config.js
+++ calculator/calculator-front-end/babel.config.js
@@ -1,6 +1,13 @@
-module.exports = function (api) {
-    api.cache(true);
-    return {
-      presets: ['babel-preset-expo', '@babel/preset-env', '@babel/preset-typescript']
-    };
-  };
+module.exports = {
+  presets: [
+    'babel-preset-expo',  // If you're using Expo
+    '@babel/preset-env',  // For modern JavaScript support
+    '@babel/preset-react', // If you're using React/React Native
+    '@babel/preset-typescript' // If you use TypeScript
+  ],
+  plugins: [
+    ['@babel/plugin-transform-class-properties', { loose: true }],
+    ['@babel/plugin-transform-private-methods', { loose: true }],
+    ['@babel/plugin-transform-private-property-in-object', { loose: true }]
+  ]
+};
