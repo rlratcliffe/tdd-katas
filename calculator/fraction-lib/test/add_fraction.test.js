e44e6e01f99481039b026d4dd59bcd42ac998385
Rob Ratcliffe <rlr@tcliffe.dev>
2024-11-21 21:13:21 -0500

    clarify test groups of add and fractions equality
    this way they will show up more clearly as separate in the test list

--- calculator/fraction-lib/test/add_fraction.test.js
+++ calculator/fraction-lib/test/add_fraction.test.js
@@ -1,62 +1,60 @@
 import Fraction from "../src/fraction.js";
 import { expect } from 'chai';
 
-
-describe('Get numerator', function() {
-    it('Numerator should be correct value', function(done) {
-        let fraction = new Fraction(1,1);
-        expect(fraction.getNumerator()).to.equal(1);
-        done();
-    });
-});
-
-describe('No simplification', function() {
-    it('Same denominator', function(done) {
-        expect(new Fraction(1,3).plus(new Fraction(1,3)).equals(new Fraction(2,3))).to.be.true;
-        done();
+describe('Add fractions', function() {
+    describe('Get numerator', function() {
+        it('Numerator should be correct value', function(done) {
+            let fraction = new Fraction(1,1);
+            expect(fraction.getNumerator()).to.equal(1);
+            done();
+        });
     });
 
-    it('Different denominator', function(done) {
-        expect(new Fraction(1,3).plus(new Fraction(2,5)).equals(new Fraction(11,15))).to.be.true;
-        done();
+    describe('No simplification', function() {
+        it('Same denominator', function(done) {
+            expect(new Fraction(1,3).plus(new Fraction(1,3)).equals(new Fraction(2,3))).to.be.true;
+            done();
+        });
+
+        it('Different denominator', function(done) {
+            expect(new Fraction(1,3).plus(new Fraction(2,5)).equals(new Fraction(11,15))).to.be.true;
+            done();
+        });
+
+        it('0 as numerator', function(done) {
+            expect(new Fraction(0,2).plus(new Fraction(1,2)).equals(new Fraction(1,2))).to.be.true;
+            done();
+        });
     });
 
-    it('0 as numerator', function(done) {
-        expect(new Fraction(0,2).plus(new Fraction(1,2)).equals(new Fraction(1,2))).to.be.true;
-        done();
+    describe('Simplification', function() {
+        it('Same denominator', function(done) {
+            expect(new Fraction(1,8).plus(new Fraction(3,8)).equals(new Fraction(1,2))).to.be.true;
+            done();
+        });
+
+        it('Different denominator, multiply only one side', function(done) {
+            expect(new Fraction(1,3).plus(new Fraction(1,6)).equals(new Fraction(1,2))).to.be.true;
+            done();
+        });
+
+        it('Two fractions equal whole number', function(done) {
+            expect(new Fraction(1,2).plus(new Fraction(1,2)).equals(new Fraction(1,1))).to.be.true;
+            done();
+        });
     });
-});
 
-describe('Simplification', function() {
-    it('Same denominator', function(done) {
-        expect(new Fraction(1,8).plus(new Fraction(3,8)).equals(new Fraction(1,2))).to.be.true;
-        done();
+    describe('Numbers as strings are handled', function() {
+        it('Both numerator and denominator as strings', function(done) {
+            expect(new Fraction("1","3").plus(new Fraction("1","3")).equals(new Fraction(2,3))).to.be.true;
+            done();
+        });
     });
 
-    it('Different denominator, multiply only one side', function(done) {
-        expect(new Fraction(1,3).plus(new Fraction(1,6)).equals(new Fraction(1,2))).to.be.true;
-        done();
-    });
-
-    it('Two fractions equal whole number', function(done) {
-        expect(new Fraction(1,2).plus(new Fraction(1,2)).equals(new Fraction(1,1))).to.be.true;
-        done();
-    });
-});
-
-describe('Numbers as strings are handled', function() {
-    it('Both numerator and denominator as strings', function(done) {
-        expect(new Fraction("1","3").plus(new Fraction("1","3")).equals(new Fraction(2,3))).to.be.true;
-        done();
-    });
-});
-
-
-
-
-describe('Print fraction', function() {
-    it('1/3', function(done) {
-        expect(new Fraction(1,3).toString()).equals("1/3")
-        done();
+    describe('Print fraction', function() {
+        it('1/3', function(done) {
+            expect(new Fraction(1,3).toString()).equals("1/3")
+            done();
+        });
     });
 });
