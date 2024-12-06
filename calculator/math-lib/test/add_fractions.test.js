79f476582eb3c4999ba86263798870ab5541e74a
Rob Ratcliffe <rlr@tcliffe.dev>
2024-12-05 21:52:46 -0500

    replace test scaffold methods with equals

--- calculator/math-lib/test/add_fractions.test.js
+++ calculator/math-lib/test/add_fractions.test.js
@@ -4,9 +4,8 @@ import { expect } from 'chai';
 describe('Adding fractions', function() {
 
     it('Add positive common denominator fractions', function(done) {
-        let sum = new Fraction(1,5).plus(new Fraction(2,5));
-        expect(sum.getNumerator()).to.equal(3);
-        expect(sum.getDenominator()).to.equal(5);
+        let sum = new Fraction(1,5).plus(new Fraction(2,5))
+        expect(sum.equals(new Fraction(3,5))).to.be.true;
         done();
     });
 });
--- calculator/math-lib/test/add_fractions_as_integers.test.js
+++ calculator/math-lib/test/add_fractions_as_integers.test.js
@@ -4,49 +4,49 @@ import { expect } from 'chai';
 describe('Adding fractions as integers', function() {
     it('Zero plus zero outputs zero', function(done) {
         let sum = new Fraction(0).plus(new Fraction(0));
-        expect(sum.intValue()).to.equal(0)
+        expect(sum.equals(new Fraction(0))).to.be.true;
         done();
     });
 
     it('Non zero plus zero outputs non zero', function(done) {
         let sum = new Fraction(1).plus(new Fraction(0));
-        expect(sum.intValue()).to.equal(1);
+        expect(sum.equals(new Fraction(1))).to.be.true;
         done();
     });
 
     it('Zero plus non zero outputs non zero', function(done) {
         let sum = new Fraction(0).plus(new Fraction(3));
-        expect(sum.intValue()).to.equal(3);
+        expect(sum.equals(new Fraction(3))).to.be.true;
         done();
     });
 
     it('Positive non zero inputs and positive non zero outputs', function(done) {
         let sum = new Fraction(4).plus(new Fraction(5));
-        expect(sum.intValue()).to.equal(9);
+        expect(sum.equals(new Fraction(9))).to.be.true;
         done();
     });
 
     it('Negative plus positive outputs negative', function(done) {
         let sum = new Fraction(-7).plus(new Fraction(2));
-        expect(sum.intValue()).to.equal(-5);
+        expect(sum.equals(new Fraction(-5))).to.be.true;
         done();
     });
 
     it('Positive plus negative equals positive', function(done) {
         let sum = new Fraction(5).plus(new Fraction(-3));
-        expect(sum.intValue()).to.equal(2);
+        expect(sum.equals(new Fraction(2))).to.be.true;
         done();
     });
 
     it('Negative plus negative equals negative', function(done) {
         let sum = new Fraction(-12).plus(new Fraction(-4));
-        expect(sum.intValue()).to.equal(-16)
+        expect(sum.equals(new Fraction(-16))).to.be.true;
         done();
     });
 
     it('Negative plus postive outputs zero', function(done) {
         let sum = new Fraction(-7).plus(new Fraction(7));
-        expect(sum.intValue()).to.equal(0);
+        expect(sum.equals(new Fraction(0))).to.be.true;
         done();
     });
 });
