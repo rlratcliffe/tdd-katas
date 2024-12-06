8df1d2425de0b63cd1c405b1c153ab7c6887b831
Rob Ratcliffe <rlr@tcliffe.dev>
2024-12-05 21:53:35 -0500

    change template to use double quotes

--- calculator/math-lib/test/template.test.js
+++ calculator/math-lib/test/template.test.js
@@ -2,7 +2,7 @@ import Fraction from "../src/fraction.js";
 import { expect } from 'chai';
 
 describe("I'm just a template", function() {
-    it('', function(done) {
+    it("", function(done) {
         done();
     });
 });
