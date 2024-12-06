c7be6bd2cf159a722cd1585e3ad04cefe0feb297
Rob Ratcliffe <rlr@tcliffe.dev>
2024-12-05 21:02:54 -0500

    add NaN and undefined tests

--- calculator/math-lib/test/fractions_equal.test.js
+++ calculator/math-lib/test/fractions_equal.test.js
@@ -1,6 +1,9 @@
 import Fraction from "../src/fraction.js";
 import { expect } from 'chai';
 
+// References: 
+// JS strict equality https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Strict_equality
+// and Java equality https://docs.oracle.com/javase/8/docs/api/java/lang/Object.html#equals-java.lang.Object-
 describe('Fractions are equal', function() {
     it('Null isn\'t equal', function(done) {
         expect(new Fraction(1,5).equals(null)).to.be.false;
@@ -11,4 +14,15 @@ describe('Fractions are equal', function() {
         expect(new Fraction(1,2).equals(new Object)).to.be.false;
         done();
     });
+
+    it("NaN", function(done) {
+        expect(new Fraction(1,5).equals(NaN)).to.be.false;
+        done();
+    });
+
+    it("Undefined not equal", function(done) {
+        expect(new Fraction(1,5).equals(undefined)).to.be.false;
+        done();
+    });
+
 });
