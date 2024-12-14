6b72ca997c8689711ab7341297c6046c1f1a6f02
Rob Ratcliffe <rlr@tcliffe.dev>
2024-12-13 22:02:15 -0500

    explore gcd algorithm

--- calculator/math-lib/test/greatest_common_divisor.test.js
+++ calculator/math-lib/test/greatest_common_divisor.test.js
@@ -2,10 +2,38 @@ import Fraction from "../src/fraction.js";
 import { expect } from 'chai';
 
 describe("Greatest common divisor", function() {
-    it("One and one", function(done) {
+    it("Reflexive examples", function(done) {
         expect(gcd(1, 1)).to.be.equal(1);
+        expect(gcd(2, 2)).to.be.equal(2);
+        expect(gcd(-1, -1)).to.be.equal(-1);
         done();
     });
+
+    it("Relatively prime", function(done) {
+        expect(gcd(2, 3)).to.be.equal(1);
+        expect(gcd(4, 7)).to.be.equal(1);
+        expect(gcd(-2, -3)).to.be.equal(-1);
+        done();
+    });
+
+    it("One is multiple of the other", function(done) {
+        expect(gcd(3,9)).to.be.equal(3);
+        expect(gcd(5,30)).to.be.equal(5);
+        done();
+    });
+
+    it("Common factor", function(done) {
+        expect(gcd(6,8)).to.be.equal(2);
+        expect(gcd(49,315)).to.be.equal(7);
+        expect(gcd(-24,-28)).to.be.equal(-4);
+        done();
+    });
+
+    it("Negatives", function(done) {
+        expect(gcd(-24,28)).to.be.equal(4);
+        expect(gcd(24,-28)).to.be.equal(-4);
+        done();
+    })
 });
 
 function gcd(a, b) {
